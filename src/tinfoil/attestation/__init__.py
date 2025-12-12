e96b690d9e97532082541408755bdf03539b46d6
Sacha Servan-Schreiber <8027773+sachaservan@users.noreply.github.com>
2025-12-12 11:25:41 -0800

    chore: deprecate v1 attestation format

--- src/tinfoil/attestation/__init__.py
+++ src/tinfoil/attestation/__init__.py
@@ -1,7 +1,6 @@
 from .attestation import (
     fetch_attestation,
     verify_attestation_json,
-    verify_sev_attestation_v1,
     verify_sev_attestation_v2,
     Measurement,
     PredicateType,
@@ -10,7 +9,6 @@ from .attestation import (
 
 __all__ = [
     'fetch_attestation',
-    'verify_sev_attestation_v1',
     'verify_sev_attestation_v2',
     'verify_attestation_json',
     'Measurement',
